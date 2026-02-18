88149a186f5ed1f13137546b4727687c9e3da8cc
sabia <sabia@elmaani.com>
2026-02-18 18:39:27 -0500

    Remove Rust project files; keep JS/HTML/CSS app only

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-#[cfg(target_arch = "wasm32")]
-fn main() {
-    yew::Renderer::<ponzisim::ui::App>::new().render();
-}
-
-#[cfg(not(target_arch = "wasm32"))]
-fn main() {
-    println!("Ponzisim UI targets WebAssembly. Run with: trunk serve --open");
-}
